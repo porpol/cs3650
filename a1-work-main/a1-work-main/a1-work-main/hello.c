af0d8964acddbf6405bb25e032af2ab7aebc093b
porpol <tonywf15@gmail.com>
2026-09-24 21:02:54 -0400

    added for loop

--- a1-work-main/a1-work-main/a1-work-main/hello.c
+++ a1-work-main/a1-work-main/a1-work-main/hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Tony\n");
-  printf("tonywf15@gmail.com\n");
+  for(int i = 0; i < 20; i++) {
+    printf("Tony\n");
+    printf("tonywf15@gmail.com\n");
+  }
 
   return 0;
 }
